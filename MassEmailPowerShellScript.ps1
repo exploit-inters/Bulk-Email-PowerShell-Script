c798cbdff5fa15b2eb9fd4a5d5c7779bd055a00f
bipulraman <bipulraman@gmail.com>
2016-03-06 21:35:52 +0530

    MassEmailPowerShellScript
    PowerShell Script to send Email to multiple people fetching data from
    CSV file

---
+++ MassEmailPowerShellScript.ps1
@@ -0,0 +1,46 @@
+﻿$MailingList = Import-Csv E:\MailingList.csv
+
+#SMTP Server and port may differ for different email service provider
+$SMTPServer = "smtp.gmail.com"
+$SMTPPort = "587"
+
+#Your email id and password
+$Username = "email@gmail.com"
+$Password = "Password"
+
+#Iterating data from CSV mailing list and sending email to each person
+foreach ( $person in $MailingList)
+{
+    $iName = $person.Name
+    $iEmail =  $person.Email    
+    $iAddress = $person.Address
+
+    $to = $person.Email
+    $cc = "other@domain.com"
+    $subject = "Email Subject"
+    $body = @" 
+
+Hi $iName,
+
+Your address is  : $iAddress
+
+Regards,
+Your Name  
+                            
+"@    
+$attachment = "C:\test.txt"
+
+    $message = New-Object System.Net.Mail.MailMessage
+    $message.subject = $subject
+    $message.body = $body
+    $message.to.add($to)
+    $message.cc.add($cc)
+    $message.from = $username
+    #$message.attachments.add($attachment)
+
+    $smtp = New-Object System.Net.Mail.SmtpClient($SMTPServer, $SMTPPort);
+    $smtp.EnableSSL = $true
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.send($message)
+    Write-Host Mail Sent to $iName
+}
